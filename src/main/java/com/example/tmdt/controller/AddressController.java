0eee6500ad7e375771b7a7cdac7a04c741bb5a9b
NamHoaiNguyen-98 <nguyenhoainam297@gmail.com>
2023-11-07 09:56:46 +0700

    done BE address + image

--- src/main/java/com/example/tmdt/controller/AddressController.java
+++ src/main/java/com/example/tmdt/controller/AddressController.java
@@ -3,7 +3,6 @@ package com.example.tmdt.controller;
 import com.example.tmdt.dto.AddressDTO;
 import com.example.tmdt.service.impl.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/example/tmdt/service/impl/WardsService.java
+++ src/main/java/com/example/tmdt/service/impl/WardsService.java
@@ -1,11 +1,20 @@
 package com.example.tmdt.service.impl;
 
 import com.example.tmdt.dto.WardsDTO;
+import com.example.tmdt.mapper.WardsMapper;
+import com.example.tmdt.model.address.Wards;
+import com.example.tmdt.repository.WardsRepository;
 import com.example.tmdt.service.IWardsService;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
-
+import java.util.Optional;
+@Service
 public class WardsService implements IWardsService {
+    @Autowired
+    private WardsMapper wardsMapper;
+    @Autowired
+    private WardsRepository wardsRepository;
     @Override
     public void save(WardsDTO dto) {
     }
@@ -17,11 +26,22 @@ public class WardsService implements IWardsService {
 
     @Override
     public WardsDTO findOne(Long id) {
-        return null;
+        Optional<Wards> optionalWards = wardsRepository.findById(id);
+        if (optionalWards.isPresent()) {
+            Wards wards = optionalWards.get();
+            wardsMapper.toDto(wards);
+        } return null;
     }
 
     @Override
     public List<WardsDTO> findAll() {
-        return null;
+        List<Wards> wards = wardsRepository.findAll();
+       return wardsMapper.toDto(wards);
+    }
+
+    @Override
+    public List<WardsDTO> findAllByIdDistrict(Long id) {
+        List<Wards> wards = wardsRepository.findALlByIdDistrict(id);
+        return wardsMapper.toDto(wards);
     }
 }
