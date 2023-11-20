f14849b1ac18e2e915d934c92981012a61b79f7b
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-11-20 16:13:01 +0700

    save reply

--- src/main/java/com/example/tmdt/controller/CommentController.java
+++ src/main/java/com/example/tmdt/controller/CommentController.java
@@ -13,29 +13,46 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     @Autowired
     private ICommentService commentService;
+
     @GetMapping
-    public ResponseEntity<?> findAllComment(){
+    public ResponseEntity<?> findAllComment() {
         return new ResponseEntity<>(commentService.findAll(), HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> findOne(@PathVariable Long id){
+    public ResponseEntity<?> findOne(@PathVariable Long id) {
         CommentDTO commentDTO = commentService.findOne(id);
-        if (commentDTO!= null){
-            return  new ResponseEntity<>(commentDTO,HttpStatus.OK);
-        } return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if (commentDTO != null) {
+            return new ResponseEntity<>(commentDTO, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
     @PostMapping()
-    public ResponseEntity<?> saveComment(@RequestBody CommentDTO commentDTO){
+    public ResponseEntity<?> saveComment(@RequestBody CommentDTO commentDTO) {
         commentService.save(commentDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @PostMapping("/reply")
+    public ResponseEntity<?> reply(@RequestParam Long id, @RequestParam String reply) {
+        CommentDTO commentDTO = commentService.findOne(id);
+        if (commentDTO != null) {
+            commentDTO.setReply(reply);
+            commentService.save(commentDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteCmt(@PathVariable Long id){
+    public ResponseEntity<?> deleteCmt(@PathVariable Long id) {
         commentService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
     @GetMapping("/product/{id}")
-    public ResponseEntity<?> showCommentByProductId(@PathVariable Long id){
-        return new ResponseEntity<>(commentService.findByIdProduct(id),HttpStatus.OK);
+    public ResponseEntity<?> showCommentByProductId(@PathVariable Long id) {
+        return new ResponseEntity<>(commentService.findByIdProduct(id), HttpStatus.OK);
     }
 }
