d6460da23405999911f734c6330b594efce08bb3
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-11-27 22:21:07 +0700

    fix comment

--- src/main/java/com/example/tmdt/repository/CommentRepository.java
+++ src/main/java/com/example/tmdt/repository/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.example.tmdt.repository;
 import com.example.tmdt.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByProduct_Id(Long id);
+    List<Comment> findAllByAccount_Id(Long id);
+    @Query(value = "select * from comment where account_id = :idAccount and product_id = :idProduct",nativeQuery = true)
+    Optional <Comment> findComment(@Param("idAccount")Long idAccount, @Param("idProduct")Long idProduct);
 }
