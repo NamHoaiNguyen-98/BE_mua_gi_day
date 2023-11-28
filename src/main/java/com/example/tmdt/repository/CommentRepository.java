cea98bae98ffaba8bced3b557e4610b6300044f3
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-11-28 08:52:27 +0700

    done comment ma giao

--- src/main/java/com/example/tmdt/repository/CommentRepository.java
+++ src/main/java/com/example/tmdt/repository/CommentRepository.java
@@ -8,7 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByProduct_Id(Long id);
+    @Query(value = "SELECT c.* FROM comment c JOIN (SELECT user_id, MAX(id) AS max_id FROM Comment WHERE product_id = :productId GROUP BY user_id) latest_comments ON c.user_id = latest_comments.user_id AND c.id = latest_comments.max_id WHERE c.product_id = :productId",nativeQuery = true)
+    List<Comment> findAllByProduct_Id(@Param("productId")Long productId);
     List<Comment> findAllByAccount_Id(Long id);
     @Query(value = "select * from comment where account_id = :idAccount and product_id = :idProduct",nativeQuery = true)
     Optional <Comment> findComment(@Param("idAccount")Long idAccount, @Param("idProduct")Long idProduct);
